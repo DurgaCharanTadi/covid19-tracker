e6f9170315eee5d28feb9a5e434df697c32112ad
Durga Charan Tadi <durgacharan.tinku@gmail.com>
2020-05-04 23:28:34 -0700

    Updates

---
+++ precache-manifest.4f0057ddbe3b2546054fe9d88480affb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "148649cd0e0e952fad056f50eec10700",
+    "url": "/covid19-tracker/index.html"
+  },
+  {
+    "revision": "3a1331f57449304b5562",
+    "url": "/covid19-tracker/static/css/main.1f9c109f.chunk.css"
+  },
+  {
+    "revision": "3e419d996492008ae1fd",
+    "url": "/covid19-tracker/static/js/2.823bc7f7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid19-tracker/static/js/2.823bc7f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a1331f57449304b5562",
+    "url": "/covid19-tracker/static/js/main.891e74f8.chunk.js"
+  },
+  {
+    "revision": "acd01a2ce132bb281fa5",
+    "url": "/covid19-tracker/static/js/runtime-main.3ee0972c.js"
+  },
+  {
+    "revision": "4e80ec5eae784a00044ed2679e4842e3",
+    "url": "/covid19-tracker/static/media/covid19-white.4e80ec5e.png"
+  },
+  {
+    "revision": "b0e9489657b44e5ffb0623badd725781",
+    "url": "/covid19-tracker/static/media/logo.b0e94896.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/DurgaCharanTadi/covid19-tracker/precache-manifest.8b00a2ef63cc9adf0a79086ab292da18.js"
+  "/covid19-tracker/precache-manifest.4f0057ddbe3b2546054fe9d88480affb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/DurgaCharanTadi/covid19-tracker/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/covid19-tracker/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
