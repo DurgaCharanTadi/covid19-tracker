d7ee6845c778bdcf659d9e99d8ec8afe4f71d0c8
Durga Charan Tadi <durgacharan.tinku@gmail.com>
2020-05-04 23:27:15 -0700

    Updates

---
+++ precache-manifest.8b00a2ef63cc9adf0a79086ab292da18.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff49d6e2d43d5be5ec59ada54a2fbf25",
+    "url": "/DurgaCharanTadi/covid19-tracker/index.html"
+  },
+  {
+    "revision": "ea505064973d6b1d9e6b",
+    "url": "/DurgaCharanTadi/covid19-tracker/static/css/main.1f9c109f.chunk.css"
+  },
+  {
+    "revision": "3e419d996492008ae1fd",
+    "url": "/DurgaCharanTadi/covid19-tracker/static/js/2.823bc7f7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/DurgaCharanTadi/covid19-tracker/static/js/2.823bc7f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea505064973d6b1d9e6b",
+    "url": "/DurgaCharanTadi/covid19-tracker/static/js/main.755c8c7c.chunk.js"
+  },
+  {
+    "revision": "cca92772a2093706e8ff",
+    "url": "/DurgaCharanTadi/covid19-tracker/static/js/runtime-main.6c2ea929.js"
+  },
+  {
+    "revision": "4e80ec5eae784a00044ed2679e4842e3",
+    "url": "/DurgaCharanTadi/covid19-tracker/static/media/covid19-white.4e80ec5e.png"
+  },
+  {
+    "revision": "b0e9489657b44e5ffb0623badd725781",
+    "url": "/DurgaCharanTadi/covid19-tracker/static/media/logo.b0e94896.png"
+  }
+]);
