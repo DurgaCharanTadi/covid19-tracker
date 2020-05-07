305733b7ee0020e8ebd0c4b15ae02a2158e84296
Durga Charan Tadi <durgacharan.tinku@gmail.com>
2020-05-06 21:11:19 -0700

    06-May

--- src/App.js
+++ src/App.js
@@ -11,6 +11,44 @@ function App() {
         <img src={require('./images/covid19-white.png')} alt="covid 19" id="covidLogo" />
       </div>
       <div className="content">
+        <div className="main_content">
+            <div className="countries_list">
+              <table>
+              <tr><th>Countries</th></tr>
+              <tr><td>USA</td></tr>
+              <tr><td>UK</td></tr>
+              <tr><td>India</td></tr>
+              <tr><td>USA</td></tr>
+              <tr><td>UK</td></tr>
+              <tr><td>India</td></tr>
+              <tr><td>USA</td></tr>
+              <tr><td>UK</td></tr>
+              <tr><td>India</td></tr>
+              </table>
+            </div>
+            <div className="cases_blocks">
+              <div className="cases_blocks_1">
+                <div className="total_number_cases">
+                  <p>Total Number Cases</p>
+                  <p>1,125,676</p>
+                </div>
+                <div className="active_cases">
+                  <p>Active Cases</p>
+                  <p>125,676</p>
+                </div>
+              </div>
+              <div className="cases_blocks_2">
+                <div className="total_deaths">
+                  <p>Total Deaths</p>
+                  <p>5,676</p>
+                </div>
+                <div className="recovered_cases">
+                  <p>Recovered Cases</p>
+                  <p>115,676</p>
+                </div>
+              </div>
+            </div>
+          </div>
       </div>
     </div>
   );
