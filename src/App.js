e8619064218f16da4f697669af22cfb94b40a337
Durga Charan Tadi <durgacharan.tinku@gmail.com>
2020-05-10 23:00:36 -0700

    10May

--- src/App.js
+++ src/App.js
@@ -57,13 +57,14 @@ function App() {
         <img src={require('./images/covid19-white.png')} alt="covid 19" id="covidLogo" />
       </div>
       <div className="content">
-      <input type="text" placeholder="Search for the Country"
-              onChange={e =>setQuery(e.target.value)}
-              value={query}
-              onKeyPress = {search}/>
 
         <div className="main_content">
         <div>
+        <input type="text" placeholder="Search for the Country"
+                className="search-bar"
+                onChange={e =>setQuery(e.target.value)}
+                value={query}
+                onKeyPress = {search}/>
         {(query == "" )? (
           <Card className="countries_list">
              <Card.Img variant="top"  src= {require('./images/world_map.png')}/>
@@ -74,11 +75,11 @@ function App() {
              </Card.Body>
              <ListGroup className="list-group-flush">
                <ListGroupItem>Cases per One Million: <NumberFormat value={alldetails.casesPerOneMillion} displayType={'text'} thousandSeparator={true} /></ListGroupItem>
-               <ListGroupItem>Deaths per One Million: {alldetails.deathsPerOneMillion}</ListGroupItem>
+               <ListGroupItem>Deaths per One Million: <NumberFormat value={alldetails.deathsPerOneMillion} displayType={'text'} thousandSeparator={true} /></ListGroupItem>
                <ListGroupItem>Total Tests: <NumberFormat value={alldetails.tests} displayType={'text'} thousandSeparator={true} /></ListGroupItem>
-               <ListGroupItem>Tests per One Million: {alldetails.testsPerOneMillion}</ListGroupItem>
-               <ListGroupItem>New Cases: {alldetails.todayCases}</ListGroupItem>
-               <ListGroupItem>New Deaths: {alldetails.todayDeaths}</ListGroupItem>
+               <ListGroupItem>Tests per One Million: <NumberFormat value={alldetails.testsPerOneMillion} displayType={'text'} thousandSeparator={true} /></ListGroupItem>
+               <ListGroupItem>New Cases: <NumberFormat value={alldetails.todayCases} displayType={'text'} thousandSeparator={true} /></ListGroupItem>
+               <ListGroupItem>New Deaths: <NumberFormat value={alldetails.todayDeaths} displayType={'text'} thousandSeparator={true} /></ListGroupItem>
              </ListGroup>
           </Card>
             ) : (
@@ -90,12 +91,12 @@ function App() {
                    </Card.Text>
                  </Card.Body>
                  <ListGroup className="list-group-flush">
-                   <ListGroupItem>Cases per One Million: {countrydetails.casesPerOneMillion}</ListGroupItem>
-                   <ListGroupItem>Deaths per One Million: {countrydetails.deathsPerOneMillion}</ListGroupItem>
-                   <ListGroupItem>Total Tests: {countrydetails.tests}</ListGroupItem>
-                   <ListGroupItem>Tests per One Million: {countrydetails.testsPerOneMillion}</ListGroupItem>
-                   <ListGroupItem>New Cases: {countrydetails.todayCases}</ListGroupItem>
-                   <ListGroupItem>New Deaths: {countrydetails.todayDeaths}</ListGroupItem>
+                   <ListGroupItem>Cases per One Million: <NumberFormat value={alldetails.casesPerOneMillion} displayType={'text'} thousandSeparator={true} /></ListGroupItem>
+                   <ListGroupItem>Deaths per One Million: <NumberFormat value={alldetails.deathsPerOneMillion} displayType={'text'} thousandSeparator={true} /></ListGroupItem>
+                   <ListGroupItem>Total Tests: <NumberFormat value={alldetails.tests} displayType={'text'} thousandSeparator={true} /></ListGroupItem>
+                   <ListGroupItem>Tests per One Million: <NumberFormat value={alldetails.testsPerOneMillion} displayType={'text'} thousandSeparator={true} /></ListGroupItem>
+                   <ListGroupItem>New Cases: <NumberFormat value={alldetails.todayCases} displayType={'text'} thousandSeparator={true} /></ListGroupItem>
+                   <ListGroupItem>New Deaths: <NumberFormat value={alldetails.todayDeaths} displayType={'text'} thousandSeparator={true} /></ListGroupItem>
                    </ListGroup>
               </Card>
           )}
@@ -106,22 +107,22 @@ function App() {
               <div className="cases_blocks_1">
                 <div className="total_number_cases">
                   <p>Confirmed Cases</p>
-                  <p>{alldetails.cases}</p>
+                  <p><NumberFormat value={alldetails.cases} displayType={'text'} thousandSeparator={true} /></p>
                 </div>
                 <div className="active_cases">
                   <p>Active Cases</p>
-                  <p>{alldetails.active}</p>
+                  <p><NumberFormat value={alldetails.active} displayType={'text'} thousandSeparator={true} /></p>
                 </div>
               </div>
 
               <div className="cases_blocks_2">
                 <div className="total_deaths">
                   <p>Total Deaths</p>
-                  <p>{alldetails.deaths}</p>
+                  <p><NumberFormat value={alldetails.deaths} displayType={'text'} thousandSeparator={true} /></p>
                 </div>
                 <div className="recovered_cases">
                   <p>Recovered Cases</p>
-                  <p>{alldetails.recovered}</p>
+                  <p><NumberFormat value={alldetails.recovered} displayType={'text'} thousandSeparator={true} /></p>
               </div>
               </div>
 
@@ -131,22 +132,22 @@ function App() {
             <div className="cases_blocks_1">
               <div className="total_number_cases">
                 <p>Confirmed Cases</p>
-                <p>{countrydetails.cases}</p>
+                <p><NumberFormat value={countrydetails.cases} displayType={'text'} thousandSeparator={true} /></p>
               </div>
               <div className="active_cases">
                 <p>Active Cases</p>
-                <p>{countrydetails.active}</p>
+                <p><NumberFormat value={countrydetails.active} displayType={'text'} thousandSeparator={true} /></p>
               </div>
             </div>
 
             <div className="cases_blocks_2">
               <div className="total_deaths">
                 <p>Total Deaths</p>
-                <p>{countrydetails.deaths}</p>
+                <p><NumberFormat value={countrydetails.deaths} displayType={'text'} thousandSeparator={true} /></p>
               </div>
               <div className="recovered_cases">
                 <p>Recovered Cases</p>
-                <p>{countrydetails.recovered}</p>
+                <p><NumberFormat value={countrydetails.recovered} displayType={'text'} thousandSeparator={true} /></p>
             </div>
             </div>
 
